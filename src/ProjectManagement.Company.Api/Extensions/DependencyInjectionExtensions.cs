8566b2e06a6a1235145e8918cdd521aa78e0dde7
afroze9 <afrozeamjad@gmail.com>
2023-05-05 16:59:35 +0500

    HC Dashboard works in docker

--- src/ProjectManagement.Company.Api/Extensions/DependencyInjectionExtensions.cs
+++ src/ProjectManagement.Company.Api/Extensions/DependencyInjectionExtensions.cs
@@ -21,7 +21,6 @@ using Steeltoe.Management.Endpoint;
 using Steeltoe.Management.Endpoint.Health;
 using Steeltoe.Management.Endpoint.Info;
 using Steeltoe.Management.Endpoint.Refresh;
-using Winton.Extensions.Configuration.Consul;
 
 namespace ProjectManagement.CompanyAPI.Extensions;
 
@@ -30,52 +29,15 @@ public static class DependencyInjectionExtensions
 {
     private static readonly string[] Actions = { "read", "write", "update", "delete" };
 
-    public static void AddConsulKv(this IConfigurationBuilder builder, ConsulKVSettings settings)
+    private static void AddActuators(this IServiceCollection services, IConfiguration configuration)
     {
-        builder.AddConsul(settings.Key, options =>
-        {
-            options.ConsulConfigurationOptions = config =>
-            {
-                config.Address = new Uri(settings.Url);
-                config.Token = settings.Token;
-            };
-
-            options.Optional = false;
-            options.ReloadOnChange = true;
-        });
+        services.AddHealthActuator(configuration);
+        services.AddInfoActuator(configuration);
+        services.AddHealthChecks();
+        services.AddRefreshActuator();
+        services.ActivateActuatorEndpoints();
     }
-
-    private static void AddPersistence(this IServiceCollection services, IConfiguration configuration)
-    {
-        services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
-        services.AddScoped(typeof(IReadRepository<>), typeof(EfRepository<>));
-        services.AddDbContext<ApplicationDbContext>(options =>
-        {
-            options.UseNpgsql(configuration);
-        });
-
-        services.AddPostgresHealthContributor(configuration);
-    }
-
-    private static void AddSecurity(this IServiceCollection services, IConfiguration configuration)
-    {
-        Auth0Settings auth0Settings = new ();
-        configuration.GetRequiredSection(nameof(Auth0Settings)).Bind(auth0Settings);
-
-        services.AddAuthentication(options =>
-        {
-            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-        }).AddJwtBearer(options =>
-        {
-            options.Authority = auth0Settings.Authority;
-            options.Audience = auth0Settings.Audience;
-        });
-
-        services.AddAuthorization(options => { options.AddCrudPolicies("company"); });
-        services.AddSingleton<IAuthorizationHandler, ScopeRequirementHandler>();
-    }
-
+    
     private static void AddApiDocumentation(this IServiceCollection services)
     {
         services.AddEndpointsApiExplorer();
@@ -115,12 +77,87 @@ public static class DependencyInjectionExtensions
             });
         });
     }
-
+    
     private static void AddApplicationServices(this IServiceCollection services)
     {
         services.AddScoped<ICompanyService, CompanyService>();
         services.AddScoped<ITagService, TagService>();
         services.AddScoped<IDomainEventDispatcher, DomainEventDispatcher>();
+    }
+    
+    private static void AddConsulDiscovery(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDiscoveryClient(configuration);
+
+        services.AddHttpContextAccessor();
+        services
+            .AddHttpClient("projects")
+            .AddServiceDiscovery()
+            .ConfigureHttpClient((serviceProvider, options) =>
+            {
+                IHttpContextAccessor httpContextAccessor = serviceProvider.GetRequiredService<IHttpContextAccessor>();
+
+                if (httpContextAccessor.HttpContext == null)
+                {
+                    return;
+                }
+
+                string? bearerToken = httpContextAccessor.HttpContext.Request.Headers["Authorization"]
+                    .FirstOrDefault(h =>
+                        !string.IsNullOrEmpty(h) &&
+                        h.StartsWith("bearer ", StringComparison.InvariantCultureIgnoreCase));
+
+                if (!string.IsNullOrEmpty(bearerToken))
+                {
+                    options.DefaultRequestHeaders.Add("Authorization", bearerToken);
+                }
+            })
+            .ConfigurePrimaryHttpMessageHandler(() =>
+            {
+                return new HttpClientHandler
+                {
+                    ClientCertificateOptions = ClientCertificateOption.Manual,
+                    ServerCertificateCustomValidationCallback = (_, _, _, _) => true,
+                };
+            })
+            .AddTypedClient<IProjectService, ProjectService>();
+    }
+    
+    private static void AddCrudPolicies(this AuthorizationOptions options, string resource)
+    {
+        foreach (string action in Actions)
+        {
+            options.AddPolicy($"{action}:{resource}",
+                policy => policy.Requirements.Add(new ScopeRequirement($"{action}:{resource}")));
+        }
+    }
+
+    private static void AddPersistence(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
+        services.AddScoped(typeof(IReadRepository<>), typeof(EfRepository<>));
+        services.AddDbContext<ApplicationDbContext>(options => { options.UseNpgsql(configuration); });
+
+        services.AddPostgresHealthContributor(configuration);
+    }
+
+    private static void AddSecurity(this IServiceCollection services, IConfiguration configuration)
+    {
+        Auth0Settings auth0Settings = new ();
+        configuration.GetRequiredSection(nameof(Auth0Settings)).Bind(auth0Settings);
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options =>
+        {
+            options.Authority = auth0Settings.Authority;
+            options.Audience = auth0Settings.Audience;
+        });
+
+        services.AddAuthorization(options => { options.AddCrudPolicies("company"); });
+        services.AddSingleton<IAuthorizationHandler, ScopeRequirementHandler>();
     }
 
     private static void AddTelemetry(this IServiceCollection services, IConfiguration configuration)
@@ -166,74 +203,18 @@ public static class DependencyInjectionExtensions
                     .AddOtlpExporter(options => { options.Endpoint = new Uri(telemetrySettings.Endpoint); });
             });
     }
-
-    private static void AddConsulDiscovery(this IServiceCollection services, IConfiguration configuration)
-    {
-        services.AddDiscoveryClient(configuration);
-
-        services.AddHttpContextAccessor();
-        services
-            .AddHttpClient("projects")
-            .AddServiceDiscovery()
-            .ConfigureHttpClient((serviceProvider, options) =>
-            {
-                IHttpContextAccessor httpContextAccessor = serviceProvider.GetRequiredService<IHttpContextAccessor>();
-
-                if (httpContextAccessor.HttpContext == null)
-                {
-                    return;
-                }
-
-                string? bearerToken = httpContextAccessor.HttpContext.Request.Headers["Authorization"]
-                    .FirstOrDefault(h =>
-                        !string.IsNullOrEmpty(h) &&
-                        h.StartsWith("bearer ", StringComparison.InvariantCultureIgnoreCase));
-
-                if (!string.IsNullOrEmpty(bearerToken))
-                {
-                    options.DefaultRequestHeaders.Add("Authorization", bearerToken);
-                }
-            })
-            .ConfigurePrimaryHttpMessageHandler(() =>
-            {
-                return new HttpClientHandler
-                {
-                    ClientCertificateOptions = ClientCertificateOption.Manual,
-                    ServerCertificateCustomValidationCallback = (_, _, _, _) => true,
-                };
-            })
-            .AddTypedClient<IProjectService, ProjectService>();
-    }
-
-    private static void AddActuators(this IServiceCollection services, IConfiguration configuration)
-    {
-        services.AddHealthActuator(configuration);
-        services.AddInfoActuator(configuration);
-        services.AddHealthChecks();
-        services.AddRefreshActuator();
-        services.ActivateActuatorEndpoints();
-    }
-
-    private static void AddCrudPolicies(this AuthorizationOptions options, string resource)
-    {
-        foreach (string action in Actions)
-        {
-            options.AddPolicy($"{action}:{resource}",
-                policy => policy.Requirements.Add(new ScopeRequirement($"{action}:{resource}")));
-        }
-    }
-
+    
     public static void RegisterDependencies(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddActuators(configuration);
         services.AddApiDocumentation();
         services.AddApplicationServices();
         services.AddAutoMapper(typeof(CompanyProfile));
+        services.AddConsulDiscovery(configuration);
         services.AddControllers(options => { options.Filters.Add<LoggingFilter>(); });
         services.AddMediatR(options => options.RegisterServicesFromAssembly(typeof(Program).Assembly));
         services.AddPersistence(configuration);
         services.AddSecurity(configuration);
-        services.AddConsulDiscovery(configuration);
         services.AddTelemetry(configuration);
         services.AddValidatorsFromAssemblyContaining(typeof(Program));
 
